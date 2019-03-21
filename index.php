b260f1430759e620377afb6da4d34d3165afbfa8
Martyna <martyna.bikulciute@gmail.com>
2019-03-22 00:11:07 +0200

    Added output new line

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $strict ='Nfq\Akademija\Strict\CalculateHomeWorkSum: ';
 $not_typed = 'Nfq\Akademija\Not_Typed\CalculateHomeWorkSum: ';
 
 
-dump($root . calculateHomeWorkSum(3, 2.2, '1')); //ats: 6.2
-dump($not_typed . Nfq\Akademija\Not_Typed\calculateHomeWorkSum(3, 2.2, '1'));
-dump($soft . Nfq\Akademija\Soft\calculateHomeWorkSum(3, 2.2, '1'));
-dump($strict . Nfq\Akademija\Strict\calculateHomeWorkSum(3, 2.2, '1'));
+echo($root . calculateHomeWorkSum(3, 2.2, '1'). PHP_EOL);
+echo($not_typed . Nfq\Akademija\Not_Typed\calculateHomeWorkSum(3, 2.2, '1'). PHP_EOL);
+echo($soft . Nfq\Akademija\Soft\calculateHomeWorkSum(3, 2.2, '1'). PHP_EOL);
+echo($strict . Nfq\Akademija\Strict\calculateHomeWorkSum(3, 2.2, '1'). PHP_EOL);
